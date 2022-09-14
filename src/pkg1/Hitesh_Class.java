181e42a8b829d2e3e05a42ad5718d3bf26766f86
Hitesh Kumar Sharma <hiteshupes@gmail.com>
2022-09-14 10:20:12 +0530

    My Java Project

---
+++ src/pkg1/Hitesh_Class.java
@@ -0,0 +1,7 @@
+package pkg1;
+public class Hitesh_Class {
+	public void sayHello()
+	{
+		System.out.println("Hello DevOps 2021-25 Batch");
+	}
+}
---
+++ src/pkg1/MasterClass.java
@@ -0,0 +1,7 @@
+package pkg1;
+public class MasterClass {
+	public static void main(String[] args) {		
+		Hitesh_Class hks = new Hitesh_Class();
+		hks.sayHello();
+	}
+}
