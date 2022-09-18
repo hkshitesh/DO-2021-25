5ffa9bc60c95ca4bf68f694f8a6f16f906bdf729
Mohak Bajaj <bmohak87@gmail.com>
2022-09-18 19:16:46 +0530

    Calculator added via pkg2 by Mohak_Bajaj_500093079

---
+++ src/pkg2/Main.java
@@ -0,0 +1,10 @@
+package pkg2;
+
+import pkg2.Calculator;
+
+public class Main{
+    public static void main(String args[]){
+        Calculator c = new Calculator();
+        c.calculator();
+    }
+}
