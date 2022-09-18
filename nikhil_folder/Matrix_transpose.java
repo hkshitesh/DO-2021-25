77639d46af84f92be9fc254c590c770de9ac5749
Nikhil <nikhilbisht0026@gmail.com>
2022-09-18 19:02:44 +0530

    Nikhil added code for matrix transpose

--- nikhil_folder/Matrix_transpose.java
+++ nikhil_folder/Matrix_transpose.java
@@ -1,19 +1,8 @@
 import java.util.*;
 public class Matrix_transpose {
-    public static void main(String args[]){
-        Scanner sc= new Scanner(System.in);
-        System.out.print("Enter the rows: ");
-        int m= sc.nextInt();
-        System.out.print("Enter the columns: ");
-        int n= sc.nextInt();
 
-        int matrix[][]=new int[m][n];
-        System.out.println("Enter the elements:");
-        for(int i=0; i<m; i++){
-           for(int j=0; j<n; j++){
-                matrix[i][j]=sc.nextInt();
-            }
-        }   
+    public void transpose(int matrix[][], int m, int n){
+        
         System.out.println("original Matrix: ");
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
@@ -28,5 +17,23 @@ public class Matrix_transpose {
             }
             System.out.println();
         }         
+    }
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the rows: ");
+        int m= sc.nextInt();
+        System.out.print("Enter the columns: ");
+        int n= sc.nextInt();
+
+        int matrix[][]=new int[m][n];
+        System.out.println("Enter the elements:");
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                matrix[i][j]=sc.nextInt();
+            }
+        }   
+
+        Matrix_transpose m1=new Matrix_transpose();
+        m1.transpose(matrix,m,n);
     }
 }
