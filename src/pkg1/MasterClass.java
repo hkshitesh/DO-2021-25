4cb7511dae6f75004b2c8f05c949188b30c59250
Vasu Kansal <vasu12b8075@gmail.com>
2022-09-18 19:01:30 +0530

    Vasu commit

--- src/pkg1/MasterClass.java
+++ src/pkg1/MasterClass.java
@@ -1,9 +1,12 @@
 package pkg1;
+
 public class MasterClass {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		Hitesh_Class hks = new Hitesh_Class();
 		hks.sayHello();
 		abcd ob2 = new abcd();
 		ob2.f1();
+		Vasu v1 = new Vasu();
+		v1._500094156();
 	}
 }
