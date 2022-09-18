a81319871a6b942136ab280a82bf70c53f8767bd
Sanskar Sisodia <500091107@stu.upes.ac.in>
2022-09-18 15:48:59 +0530

    Added java project

--- src/pkg1/MasterClass.java
+++ src/pkg1/MasterClass.java
@@ -1,9 +1,14 @@
 package pkg1;
+
 public class MasterClass {
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		Hitesh_Class hks = new Hitesh_Class();
 		hks.sayHello();
 		abcd ob2 = new abcd();
 		ob2.f1();
+		Sanskar_class ss = new Sanskar_class();
+		int[] numbers;
+		numbers = ss.input_digit();
+		ss.checking_digit(numbers);
 	}
 }
