b759b86dff5fba89a7b43a7edc69677de734ae4e
Aryansh <500093038@stu.upes.ac.in>
2022-09-18 18:02:51 +0530

    Recursive preorder traversal of binary tree

--- src/pkg1/MasterClass.java
+++ src/pkg1/MasterClass.java
@@ -3,5 +3,8 @@ public class MasterClass {
 	public static void main(String[] args) {		
 		Hitesh_Class hks = new Hitesh_Class();
 		hks.sayHello();
+		Aryansh_Class ars = new Aryansh_Class();
+		ars.createBinaryTree();
+		ars.preorder(ars.root);
 	}
 }
